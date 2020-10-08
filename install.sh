50f6fc2b044c58044f60f800887af229b1808639
5+1 <59787082+langonginc@users.noreply.github.com>
2020-10-08 09:26:41 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,31 @@
+#/bin/bash
+
+echo ' LanGong ScreenShot-L Install Script'
+
+
+
+compile (){
+  echo ' >> Compile"
+  echo '    -> (0/1) Compile'
+  qmake ScreenShot.pro
+  make
+  echo '    -> (1/1) Done!'
+}
+
+installto (){
+  bin=$1
+  dir=$2
+  echo ' >> Install'
+  echo '    -> (0/1) Install bin to "'$bin'"'
+  cp ScreenShot "$bin/screenshotl"
+  cp -r ./* "$dir/screenshotl"
+  echo '    -> (1/1) Done!'
+}
+
+if [ $1 == 'dir' ]; then
+  compile
+  installto $2 $3
+else
+  compile
+  installto /usr/bin /usr/share
+fi
